d24de1416bf1cbdda8f7235706f73035a7acbdd6
wobuchiroubao <>
2021-05-01 16:50:16 +0300

    Started working on novel website: added html templates and changed some stuff in backend.py, though it doesn't save anything into db. Wonder why...

--- website/website/backend.py
+++ website/website/backend.py
@@ -41,13 +41,14 @@ def initdbc_command():
 
 @app.route('/')
 def main_page():
-    dbc = get_dbc()
+    """dbc = get_dbc()
     cur = dbc.cursor()
     cur.execute('SELECT title, text FROM entries ORDER BY id DESC')
     entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    return render_template('show_entries.html', entries=entries)"""
+    return render_template('main_page.html')
 
-@app.route('/add', methods=['POST'])
+"""@app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
@@ -56,11 +57,28 @@ def add_entry():
                  [request.form['title'], request.form['text']])
     dbc.commit()
     flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_entries'))"""
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        dbc = get_dbc()
+        cur = dbc.cursor()
+        if request.form['password'] == request.form['password_rep']:
+            cur.execute(
+                'INSERT INTO website_db.user (nickname, password, e_mail) VALUES (%s, %s, %s)',
+                [request.form['nickname'], request.form['password'],
+                request.form['e_mail'] or 'NULL']
+                )
+            dbc.commit()
+            return redirect(url_for('main_page'))
+        else:
+            pass
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
+    """error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
@@ -70,15 +88,26 @@ def login():
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+    return render_template('login.html', error=error)"""
+    return render_template('login.html')
 
-@app.route('/logout')
+"""@app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     # second parameter means that pop() will do nothing
     # if key was not in dict session
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_entries'))"""
+
+@app.route('/search')
+def search():
+    dbc = get_dbc()
+    cur = dbc.cursor()
+    cur.execute(
+        'SELECT name, description, rating FROM website_db.novel WHERE name = %s ORDER BY rating DESC',
+        [request.form['novel_name']])
+    data = cur.fetchall()
+    return render_template('novels_list.html', data=data)
 
 # decorator app.teardown_appcontext registers a function to be called
 # when the application context ends.
