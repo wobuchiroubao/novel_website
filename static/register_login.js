06e61b361d47423ce065926486e9cb61b9055d04
wobuchiroubao <>
2024-07-25 17:37:53 +0300

    Rewritten login & register js handler from XMLHttp to Fetch API

--- static/register_login.js
+++ static/register_login.js
@@ -2,25 +2,27 @@
 
 function handle_register_login(event, url, form_id, err_id) {
   event.preventDefault();
-  let xhr = new XMLHttpRequest();
-  let fd = new FormData(document.getElementById(form_id))
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4) {
-      return;
-    }
-    if (xhr.status != 200) {
-      console.log('Failed request:', xhr.status, xhr.statusText);
-      return;
-    }
-    let response = JSON.parse(xhr.responseText);
-    if (response.err) {
-      let err = document.getElementById(err_id);
-      err.style.display = 'block';
-      err.innerHTML = response.err;
-    } else {
-      location.href = response.url;
-    }
-  };
-  xhr.open('POST', url, true);
-  xhr.send(fd);
+
+  let fd = new FormData(document.getElementById(form_id));
+
+  fetch(url, {
+    method: 'POST',
+    body: fd,
+  }).then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(res => {
+      console.log(res);
+      if (res.err) {
+        let err = document.getElementById(err_id);
+        err.style.display = 'block';
+        err.innerHTML = res.err;
+      } else {
+        location.href = res.url;
+      }
+    })
+    .catch((err) => console.error(`Failed request: ${err.message}`));
 }
